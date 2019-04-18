ab72ea30cca64ea7b7de54c2046421c2d85ec737
truongan <truongan2005@gmail.com>
2019-04-18 21:24:27 +0700

    the check function

--- app.js
+++ app.js
@@ -11,7 +11,7 @@ exist = false;
 function show_button(e){
     console.log(e);
             
-    document.querySelectorAll('.btn').forEach(element => {
+    document.querySelectorAll('.item').forEach(element => {
         element.innerText = 'x';
     });
 
@@ -29,7 +29,7 @@ function new_game(){
     
     for (let index = 0; index < size; index++) {
         var card = document.createElement('div');
-        card.innerHTML = '<div class="card">        <div class="card-header">            <h5 class="card-title">box ' + index + '</h5>        </div>        <div class="card-body draggable-dropzone--occupied">            <a href="#" class="btn btn-lg btn-primary" data-val="'+ Math.floor(Math.random()*size*3 + 1) +'"> x </a>        </div>    </div>';
+        card.innerHTML = '<div class="card">        <div class="card-header">            <h5 class="card-title">box ' + index + '</h5>        </div>        <div class="card-body draggable-dropzone--occupied">            <a href="#" class="btn btn-lg btn-primary item" data-val="'+ Math.floor(Math.random()*size*3 + 1) +'"> x </a>        </div>    </div>';
         
         card.onclick = function(e){
             // show_button(e.target);
@@ -41,12 +41,53 @@ function new_game(){
     const draggable = new Draggable.Droppable(document.querySelectorAll('.card'), {
         draggable: '.btn'
         ,dropzone: '.card-body'
-      });
-      
-      draggable.on('drag:start', (e) => show_button(e.source));
-      draggable.on('drag:move', () => console.log('drag:move'));
-      draggable.on('drag:stop', (e) => show_button(e.source));
+    });
 
+    draggable.on('drag:start', (e) => show_button(e.source));
+    draggable.on('drag:move', () => console.log('drag:move'));
+    draggable.on('drag:stop', (e) => show_button(e.source));
+
+    document.querySelector('.checkbtn').onclick = function(){
+        var target = document.querySelector('.list-row'); 
+
+        var old_val;
+
+        fail = true;
+        for (let i = 0; i < target.childNodes.length; i++) {
+            const box = target.childNodes[i];
+            a = box.querySelector('.item');
+            if (a==null) {
+                document.querySelector('.modal-body').innerText = 'Too bad, you left a box empty in the main row.';
+                $('#myModal').modal('show');
+                fail = false;
+                console.log('fail 1');
+                break;
+            }
+
+            val = parseInt(a.dataset['val']);
+            console.log(val);
+            console.log(a);
+            if (i != 0){
+                if (val < old_val){
+                    console.log(document.querySelector('.modal-body').innerText );
+                    document.querySelector('.modal-body').innerText = 'Too bad, Box ' + i + ' is not in order';
+                    console.log(document.querySelector('.modal-body').innerText );
+                    $('#myModal').modal('show');
+                    console.log('fail 2');
+                    fail = false;
+                    break;
+                }
+            }
+            old_val = val; 
+        }
+
+        if (fail){
+            document.querySelector('.modal-body').innerText = 'CONGRATULATIONS. YOU DID IT';
+            $('#myModal').modal('show');
+        }
+
+        
+    }
 
 
 }
